d1545c3b53854516fb54f1d247a8c404ad0fd644
Thallys <thallysvilemos@gmail.com>
2024-11-17 23:47:05 -0300

    chore: Adicionado Angular Material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideZoneChangeDetection({ eventCoalescing: true })
+    provideZoneChangeDetection({ eventCoalescing: true }), provideAnimationsAsync()
   ]
 };
