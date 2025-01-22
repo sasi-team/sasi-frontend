97c563436c450c2e6e3e2b7e40f82a7cf483a74f
Dhones-Lacerda <dhones.l.rocha@gmail.com>
2025-01-22 20:43:45 -0300

    refactor(links): add redirect to about page

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
